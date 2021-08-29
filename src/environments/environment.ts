6ae73a1f167ac3ec8afdeecc18c340ee1c2d3274
Alex Karumbi <alex.muhia@student.moringaschool.com>
2021-08-29 13:57:02 +0300

    add image to assets

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  clientId:"4c7b292208fa61e6ce43",
+  clientSecret:"f431270de347d6326f458451bce0329dbfd99274"
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+};
