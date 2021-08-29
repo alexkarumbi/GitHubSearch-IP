347533af20a35653a2050b26a6623470b45e4121
Alex Karumbi <alex.muhia@student.moringaschool.com>
2021-08-29 14:08:53 +0300

    Add search page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { SearchpageComponent } from './searchpage/searchpage.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    SearchpageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/searchpage/searchpage.component.spec.ts
+++ src/app/searchpage/searchpage.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomepageComponent } from './homepage.component';
+import { SearchpageComponent } from './searchpage.component';
 
-describe('HomepageComponent', () => {
-  let component: HomepageComponent;
-  let fixture: ComponentFixture<HomepageComponent>;
+describe('SearchpageComponent', () => {
+  let component: SearchpageComponent;
+  let fixture: ComponentFixture<SearchpageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomepageComponent ]
+      declarations: [ SearchpageComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomepageComponent);
+    fixture = TestBed.createComponent(SearchpageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
